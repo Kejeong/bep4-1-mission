68d595abeac1750ed93484f6cc8302bf38b64334
kimeunjeong <keunjeong613@gmail.com>
2026-09-18 11:31:52 +0900

    0016/0017-BaseMember 도입

--- src/main/java/com/back/boundedContext/member/domain/Member.java
+++ src/main/java/com/back/boundedContext/member/domain/Member.java
@@ -1,15 +1,18 @@
 package com.back.boundedContext.member.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.member.domain.SourceMember;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "MEMBER_MEMBER")
 @NoArgsConstructor
 @Getter
-public class Member extends BaseIdAndTime {
+public class Member extends SourceMember {
     @Column(unique = true)
     private String username;
     private String password;
@@ -17,12 +20,17 @@ public class Member extends BaseIdAndTime {
     private int activityScore;
 
     public Member(String username, String password, String nickname) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
+        super(username, password, nickname);
     }
 
+    /**
+     * 활동점수 증가
+     * @param amount
+     * @return
+     */
     public int increaseActivityScore(int amount) {
-        return this.activityScore += amount;
+        setActivityScore(getActivityScore() + amount);
+
+        return getActivityScore();
     }
 }
