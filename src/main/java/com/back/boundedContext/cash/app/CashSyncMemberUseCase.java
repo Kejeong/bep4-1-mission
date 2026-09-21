0b5f38c32b3f42fa4906ceb103e9ab6d97db13f7
kimeunjeong <keunjeong613@gmail.com>
2026-09-21 10:23:18 +0900

    0027-cash모듈 리팩토링

--- src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
@@ -2,6 +2,9 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.cash.dto.CashMemberDto;
+import com.back.shared.cash.event.CashMemberCreatedEvent;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,21 +13,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CashSyncMemberUseCase {
     private final CashMemberRepository cashMemberRepository;
+    private final EventPublisher eventPublisher;
 
     /**
      * 회원 동기화
      */
     public CashMember syncMember(MemberDto member) {
-        CashMember cashMember = new CashMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
+        boolean isNew = !cashMemberRepository.existsById(member.getId());
+
+        CashMember _member = cashMemberRepository.save(
+                new CashMember(
+                        member.getId(),
+                        member.getCreateDate(),
+                        member.getModifyDate(),
+                        member.getUsername(),
+                        "",
+                        member.getNickname(),
+                        member.getActivityScore()
+                )
         );
 
-        return cashMemberRepository.save(cashMember);
+        if (isNew) {  // 새로 회원이 등록이 되면 이벤트 발행
+            eventPublisher.publish(new CashMemberCreatedEvent(new CashMemberDto(_member))
+            );
+        }
+
+        return _member;
     }
 }
