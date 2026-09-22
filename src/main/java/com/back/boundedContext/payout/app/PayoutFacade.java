fc0203729444388c6b61ab42b00f1e185827c8ba
kimeunjeong <keunjeong613@gmail.com>
2026-09-22 15:43:00 +0900

    0041-PayoutMember 및 이벤트 생성

---
+++ src/main/java/com/back/boundedContext/payout/app/PayoutFacade.java
@@ -0,0 +1,40 @@
+package com.back.boundedContext.payout.app;
+
+import com.back.shared.market.dto.OrderDto;
+import com.back.shared.member.dto.MemberDto;
+import com.back.shared.payout.dto.PayoutMemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PayoutFacade {
+    private final PayoutSyncMemberUseCase payoutSyncMemberUseCase;
+    private final PayoutCreatePayoutUseCase payoutCreatePayoutUseCase;
+    private final PayoutAddPayoutCandidateItemsUseCase payoutAddPayoutCandidateItemsUseCase;
+
+    /**
+     * 회원정보를 정산회원이랑 동기화한다.
+     */
+    @Transactional
+    public void syncMember(MemberDto member) {
+        payoutSyncMemberUseCase.syncMember(member);
+    }
+
+    /**
+     * 정산을 생성한다.
+     */
+    @Transactional
+    public void createPayout(PayoutMemberDto payee) {
+        payoutCreatePayoutUseCase.createPayout(payee);
+    }
+
+    /**
+     * 결제가 완료된 주문의 상품들을 정산 후보로 등록
+     */
+    @Transactional
+    public void addPayoutCandidateItems(OrderDto order) {
+        payoutAddPayoutCandidateItemsUseCase.addPayoutCandidateItems(order);
+    }
+}
---
+++ src/main/java/com/back/boundedContext/payout/in/PayoutEventListener.java
@@ -0,0 +1,57 @@
+package com.back.boundedContext.payout.in;
+
+import com.back.boundedContext.payout.app.PayoutFacade;
+import com.back.shared.market.event.MarketOrderPaymentCompletedEvent;
+import com.back.shared.member.event.MemberJoinedEvent;
+import com.back.shared.member.event.MemberModifiedEvent;
+import com.back.shared.payout.event.PayoutMemberCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
+@Component
+@RequiredArgsConstructor
+public class PayoutEventListener {
+    private final PayoutFacade payoutFacade;
+
+    /**
+     * 회원이 가입한 시점에 정산쪽회원 테이블과 동기화
+     */
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MemberJoinedEvent event) {
+        payoutFacade.syncMember(event.getMember());
+    }
+
+    /**
+     * 회원정보를 수정한 시점에 정산쪽 테이블도 회원 동기화
+     */
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MemberModifiedEvent event) {
+        payoutFacade.syncMember(event.getMember());
+    }
+
+    /**
+     * 정산 회원이 생성이 될 때 정산을 생성하는 이벤트 발생
+     */
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(PayoutMemberCreatedEvent event) {
+        payoutFacade.createPayout(event.getMember());
+    }
+
+    /**
+     * 결제 정산이 완료 되었을 때
+     * @param event
+     */
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MarketOrderPaymentCompletedEvent event) {
+        payoutFacade.addPayoutCandidateItems(event.getOrder());
+    }
+}
